e92028d88a05c6d9942e5973c0bd234f5312e9ce
Krishna Agarwal <krishnaagarwal@Krishnas-Mac-mini.local>
2026-05-20 00:31:56 +0530

    GA and clarity added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "@/app/context/cart";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -60,6 +62,10 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col bg-stone-50 text-stone-900">
         <CartProvider>{children}</CartProvider>
       </body>
+      <GoogleAnalytics gaId="G-V822G3HYXM" />
+      <Script id="clarity" strategy="afterInteractive">
+        {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y)})(window,document,"clarity","script","wtoonl4jbk");`}
+      </Script>
     </html>
   );
 }
